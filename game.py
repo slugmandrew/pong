f26105ebbd04ecd00d0361b9c987d9c3601742a4
Drew Spencer <slugmandrew@gmail.com>
2019-10-22 23:03:50 +0100

    Added bones

--- game.py
+++ game.py
@@ -14,34 +14,44 @@ WINDOWHEIGHT = 600
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation')
 
-# set up direction variables
+# load images
+background = pygame.image.load("skull_background.jpg").convert()
+background = pygame.transform.rotozoom(background, 0, 1.2)
+skull = pygame.image.load("skull.png").convert_alpha()
+skull = pygame.transform.rotozoom(skull, 0, 0.5)
+bone_left = pygame.image.load("bone_left.png").convert_alpha()
+bone_right = pygame.image.load("bone_right.png").convert_alpha()
+bone_left = pygame.transform.rotozoom(bone_left, 0, 0.45)
+bone_right = pygame.transform.rotozoom(bone_right, 0, 0.45)
+
+
+# set up direction/speed variables
+STATIONARY = 0
 DOWNLEFT = 1
-DOWNRIGHT = 3
-UPLEFT = 7
-UPRIGHT = 9
+DOWNRIGHT = 2
+UPLEFT = 3
+UPRIGHT = 4
 MOVESPEED = 5
 
 # set up the colors
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+PURPLE = (155, 0, 255)
 YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
 
 centreX = WINDOWWIDTH / 2
 centreY = WINDOWHEIGHT / 2
 
-# set up the block data structure
-# b1 = {'rect': pygame.Rect(300, 80, 50, 100), 'color': RED, 'dir': UPRIGHT}
-ball: Dict[str, Union[RectType, Tuple[int, int, int], int]] = {'rect': pygame.Rect(centreX, centreY, 20, 20), 'color': GREEN, 'dir': UPRIGHT}
-# b3 = {'rect': pygame.Rect(100, 150, 60, 60), 'color': BLUE, 'dir': DOWNLEFT}
-# balls = [b2]
+# set up the ball
 
-padddle1 = {'rect': pygame.Rect(50, 250, 20, 100), 'color': BLUE}
-padddle2 = {'rect': pygame.Rect(900, 250, 20, 100), 'color': RED}
+ball = {'rect': skull, 'dir': UPRIGHT}
+paddle1 = {'rect': pygame.Rect(50, 250, 20, 100), 'color': PURPLE}
+paddle2 = {'rect': pygame.Rect(900, 250, 20, 100), 'color': RED}
 
 # initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
-myfont = pygame.font.SysFont("monospace", 96)
+myfont = pygame.font.SysFont("Chiller", 96)
 
 # render text
 
@@ -52,7 +62,7 @@ p2_score = 0
 while True:
 
     def new_ball(direction):
-        return {'rect': pygame.Rect(centreX, centreY, 20, 20), 'color': GREEN, 'dir': direction}
+        return {'rect': skull, 'dir': direction}
 
 
     # check for the QUIT event
@@ -64,10 +74,12 @@ while True:
 
     # draw the black background onto the surface
     windowSurface.fill(BLACK)
+    windowSurface.blit(background, (0, 0))
+
 
     # loop the block
-    paddle1_rect = padddle1['rect']
-    paddle2_rect = padddle2['rect']
+    paddle1_rect = paddle1['rect']
+    paddle2_rect = paddle2['rect']
 
     # move the block data structure
     if ball['dir'] == DOWNLEFT:
@@ -109,6 +121,8 @@ while True:
         # increment score by 1
         p2_score += 1
 
+        # wait for space bar to start
+
         ball = new_ball(UPRIGHT)
 
     if ball['rect'].right > WINDOWWIDTH:
@@ -144,7 +158,7 @@ while True:
 
     # draw the block onto the surface
 
-    pygame.draw.rect(windowSurface, ball['color'], ball['rect'])
+    windowSurface.blit(ball['rect'], (ball['rect'].left, ball['rect'].top))
 
     # paddle1
 
@@ -160,12 +174,12 @@ while True:
     if keys_pressed[pygame.K_l]:
         paddle2_rect.top += MOVESPEED
 
-    pygame.draw.rect(windowSurface, padddle1['color'], paddle1_rect)
-    pygame.draw.rect(windowSurface, padddle2['color'], paddle2_rect)
+    windowSurface.blit(bone_left, (paddle1_rect.left, paddle1_rect.top))
+    windowSurface.blit(bone_right, (paddle2_rect.left, paddle2_rect.top))
 
-    windowSurface.blit(myfont.render(str(p1_score), 1, BLUE), (0, 0))
+    windowSurface.blit(myfont.render(str(p1_score), 1, PURPLE), (20, 0))
     windowSurface.blit(myfont.render(str(p2_score), 1, RED), (940, 0))
 
     # draw the window onto the screen
     pygame.display.update()
-    time.sleep(0.02)
+    time.sleep(0.01)
